71cfbab6352b26856829f4cb51c2e3587f2bea3b
Simon <simon.westfechtel@gmail.com>
2020-01-01 17:00:44 +0100

    Output generation for efficiency (so far), modification of output files (header line) via python

---
+++ Code/WriteOutput.m
@@ -0,0 +1,15 @@
+function ret = WriteOutput(name,velocity,force,pressure,distance,acceleration_neg)
+    V = [velocity.Time, velocity.Data];
+    F = [force.Time, force.Data];
+    P = [pressure.Time, pressure.Data];
+    D = [distance.Time, distance.Data];
+    A = [acceleration_neg.Time, acceleration_neg.Data];
+    mkdir(strcat('output/',name));
+    dir = strcat('output/',name);
+    writematrix(V,strcat(dir,'/Velocity.csv'));
+    writematrix(F,strcat(dir,'/Force.csv'));
+    writematrix(P,strcat(dir,'/Pressure.csv'));
+    writematrix(D,strcat(dir,'/Distance.csv'));
+    writematrix(A,strcat(dir,'/Acceleration_neg.csv'));
+    ret = 1;
+end
