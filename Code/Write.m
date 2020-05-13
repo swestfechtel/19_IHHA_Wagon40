77122d29dd7b8cd2f12efd7b594f6ad548a894b3
WESTFECHTEL-PC\Westfechtel <simon.westfechtel@gmail.com>
2020-05-13 12:01:00 +0200

    fill output file empty entries with zeros

--- Code/Write.m
+++ Code/Write.m
@@ -1,14 +1,20 @@
 function ret = Write(id,velocity,force,pressure,distance,acceleration,wagon_ids,t,u,grad,ft,fc)
 	numrows = get(force,'Length');
+	numwagons = length(wagon_ids);
 	time = force.Time;
+	filler = zeros(1,40 - numwagons);
 	% mkdir('output/');
 	matrix = [];
 
 	for i = 1:1:numrows
 		f = getdatasamples(force,i);
+		f = [f,filler];
 		p = getdatasamples(pressure,i);
+		p = [p,filler];
 		v = getdatasamples(velocity,i);
+		v = [v,filler];
 		a = getdatasamples(acceleration,i);
+		a = [a,filler];
 		% d = getdatasamples(distance,i);
 		d = 0;
 		row = [id,time(i),f,p,v,a,d,wagon_ids,grad,ft,fc]; 	% TODO: add simulation input aka track profile to matrix
