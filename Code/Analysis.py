a06a870a70f5d08ea42101ecefe53f7b094c35da
WESTFECHTEL-PC\Westfechtel <simon.westfechtel@gmail.com>
2020-08-10 10:58:02 +0200

    add file for analysis

--- Code/Analysis.py
+++ Code/Analysis.py
@@ -1,13 +1,27 @@
 import pandas
-import matplotlib.pyplot as plt
 
-tmp = pandas.read_csv('output/run1/Force.csv')
-df = tmp.set_index('Timestamp')
+# id,time(i),f,p,v,a,d,wagon_ids,grad,ft,fc
+columns = ['id', 'time']
 
-print(df.shape)
-print(df)
+for i in range(40):
+	columns.append(f"F{i}")
 
-plt.figure()
-df.plot()
-plt.autoscale()
-plt.show()
+for i in range(40):
+	columns.append(f"P{i}")
+
+columns.append('velocity')
+
+for i in range(40):
+	columns.append(f"a{i}")
+
+columns.append(distance)
+
+for i in range(40):
+	columns.append(f"Wagon{i}")
+
+columns.append('gradient')
+columns.append('Ft')
+columns.append('fc')
+
+df = pandas.read_csv('output/output.tsv', names=columns, memory_map=True)
+df.info(verbose=True)
